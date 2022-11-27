88a358d6b5b55165b5545dd8bef0704f8094d78a
Eugênio <eugenio@sisger.com.br>
2022-11-26 21:01:47 -0300

    Project created

---
+++ src/main/java/com/sisger/cursoJW/CursoJwApplication.java
@@ -0,0 +1,13 @@
+package com.sisger.cursoJW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisger/cursoJW/CursoJwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisger.cursoJW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
